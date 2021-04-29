92797df284bb74f3a826f9eb4ad46fd36957ef19
itsmarkalex <itsmarkalex@outlook.com>
2021-04-28 20:40:11 -0500

    ignorar 2

--- 02-MiPrimeraApp/src/screens/PositionScreen.tsx
+++ 02-MiPrimeraApp/src/screens/PositionScreen.tsx
@@ -5,9 +5,9 @@ export const PositionScreen = () => {
   return (
     <View style={styles.container}>
       
+      <View style={styles.cajaVerde}/>
       <View style={styles.cajaMorada}/>
       <View style={styles.cajaNaranja}/>
-      <View style={styles.cajaVerde}/>
     </View>
   )
 }
@@ -43,16 +43,16 @@ const styles = StyleSheet.create({
     right: 0
   },
   cajaVerde: {
-    width: 100,
-    height: 100,
+    /* width: 100,
+    height: 100, */
     backgroundColor: 'green',
     borderWidth: 10,
     borderColor: 'white',
     position: 'absolute',
     bottom: 0,
     left: 0,
-    /* top: 0,
-    right: 0 */
+    top: 0,
+    right: 0
     /* ...StyleSheet.absoluteFillObject */
 
   }
