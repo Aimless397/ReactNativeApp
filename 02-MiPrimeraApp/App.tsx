8fa8ab26dc3f8783902e6fbf69eccae06ac2bd78
itsmarkalex <itsmarkalex@outlook.com>
2021-04-28 20:30:05 -0500

    ignorar

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -3,17 +3,17 @@ import {SafeAreaView, View } from 'react-native'
 import { PositionScreen } from './src/screens/PositionScreen'
 /* import { DimensionesScreen } from './src/screens/DimensionesScreen' */
 /* import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen' */
-/* import { ContadorScreen } from './src/screens/ContadorScreen' */
+import { ContadorScreen } from './src/screens/ContadorScreen'
 /* import { HolaMundoScreen } from './src/screens/HolaMundoScreen' */
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       {/* <HolaMundoScreen /> */}
-      {/* <ContadorScreen /> */}
+      <ContadorScreen/>
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
-      <PositionScreen />
+      {/* <PositionScreen /> */}
     </SafeAreaView>
     
   )
